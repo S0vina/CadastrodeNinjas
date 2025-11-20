beb8e65d0dee5b7a4dbb5992ca3e938b06ebc22c
S0vina <hillan1236@gmail.com>
2025-11-20 14:24:38 -0300

    Adicao da dependecia JPA, renomeacao da classe controller e construcao da entity ninja apartir da classe NinjaModel.
    Confirmo a alteracao

---
+++ src/main/java/dev/hillanzinho/CadastrodeNinjas/NinjaModel.java
@@ -0,0 +1,50 @@
+package dev.hillanzinho.CadastrodeNinjas;
+
+import jakarta.persistence.*;
+
+import javax.swing.table.TableColumn;
+
+// Entity Ninja no BD
+@Entity
+@Table (name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String senha;
+
+    // Construtor "No args"
+    public NinjaModel() {
+    }
+
+    // Construtor "all args"
+    public NinjaModel(String nome, String email, int idade, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+        this.senha = senha;
+    }
+
+    // Getters e setters
+
+    public String getNome() {return this.nome;}
+
+    public void setNome(String nome) {this.nome = nome;}
+
+    public String getEmail() {return this.email;}
+
+    public void setEmail(String email) {this.email = email;}
+
+    public int getIdade() {return this.idade;}
+
+    public void setIdade(int idade) {this.idade = idade;}
+
+    public String getSenha() {return this.senha;}
+
+    public void setSenha(String senha) {this.senha = senha;}
+
+}
