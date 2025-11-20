14c5091b5d14df636e73980dcf31771c5151cfa1
S0vina <hillan1236@gmail.com>
2025-11-20 00:41:13 -0300

    Adicao da classe Application e do Controler para a API. O mapping ainda nao esta funcinando corretamente

---
+++ src/test/java/dev/hillanzinho/CadastrodeNinjas/CadastrodeNinjasApplication.java
@@ -0,0 +1,13 @@
+package dev.hillanzinho.CadastrodeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrodeNinjasApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(CadastrodeNinjasApplication.class, args);
+    }
+
+}
